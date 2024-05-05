ea3a08b331dae272c2cdc2717104090cb3ce0e5c
Filip Bugoš <xbugos@fi.muni.cz>
2024-05-05 16:07:41 +0200

    Implement query getting all projects with all users involved

--- src/actions/projectActions.ts
+++ src/actions/projectActions.ts
@@ -1,7 +1,7 @@
 'use server';
 
 import {db} from "../../db/db";
-import {InsertProject, project, user} from "../../db/schema";
+import {InsertProject, project, user, userProject} from "../../db/schema";
 import {and, eq, SQL} from "drizzle-orm";
 
 export const getProject = async (filters?: SQL[]) => {
@@ -11,3 +11,21 @@ export const getProject = async (filters?: SQL[]) => {
 export const createProject = async (data: InsertProject) => {
 	return await db.insert(project).values(data);
 }
+
+// Get all projects with all participants
+export const getAllUserProjects = async (filters?: SQL[]) => {
+	const projectsWithUsers =  await db
+		.select()
+		.from(userProject)
+		.leftJoin(project, eq(project.ID, userProject.Project))
+		.leftJoin(user, eq(user.ID, userProject.User))
+		.where(filters ? and(...filters) : undefined)
+		.execute();
+
+	return projectsWithUsers.map((project) => ({
+		project: {...project.Project},
+		user: {...project.User},
+	}));
+};
+
+export type ProjectsWithUsers = Awaited<ReturnType<typeof getAllUserProjects>>[number];
