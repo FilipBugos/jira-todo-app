90e7b1578f1b245d557aac1831a014149419ee61
Filip Bugoš <xbugos@fi.muni.cz>
2024-05-06 14:11:11 +0200

    Customize dialog implementation

--- src/components/create-issue-dialog.tsx
+++ src/components/create-issue-dialog.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import * as Dialog from '@radix-ui/react-dialog';
 import { SelectProject, SelectSprint, SelectUser, SelectUserProject, sprint } from '../../db/schema';
 import Select, { MultiValue, ActionMeta, SingleValue } from "react-select";
 import { Controller, FormProvider, useForm, useFormContext } from 'react-hook-form';
@@ -11,6 +10,15 @@ import { SelectOption } from './issue-filters';
 import { ProjectsWithUsers } from '@/actions/projectActions';
 import { getLabels, getStatuses } from '@/lib/utils';
 import { createIssue } from '@/actions/issueActions';
+import {
+	Dialog,
+	DialogClose,
+	DialogContent,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger
+} from '@/components/ui/dialog';
 
 type CreateIssueDialogType = {
     projects: ProjectsWithUsers[],
@@ -61,69 +69,70 @@ const CreateIssueDialog = ({projects, trigger, sprints}: CreateIssueDialogType) 
         <FormProvider {...form}>
             <form>
                 <div className=''>
-                    <Dialog.Root>
-                    <Dialog.Trigger asChild>
-                        {trigger}
-                    </Dialog.Trigger>
-                    <Dialog.Portal>
-                    <Dialog.Overlay className="DialogOverlay" />
-                    <Dialog.Content className="DialogContent bg-slate-300">
-                        <Dialog.Title className="DialogTitle text-xl mb-5">Create Issue</Dialog.Title>
-                        <div className='flex flex-col gap-4 mt-2'>
-                            <div className="grid grid-cols-2">
-                                <label>Project</label>
-                                <select {...form.register('project')} onChange={(selectedOption) => { 
-                                    const selectedProject = Number(selectedOption.target.value);
-                                    setSelectedProject(selectedProject);
-                                    setSprintsToSelect(sprints.filter(s => s.Project === selectedProject))
-                                }}>
-                                    {projects.map(p => <option key={p.project?.ID} value={p.project?.ID}>{p.project?.Name}</option>)}
-                                </select>
+                    <Dialog>
+                        <DialogTrigger asChild>
+                            {trigger}
+                        </DialogTrigger>
+                        <DialogContent className="DialogContent bg-slate-300">
+                            <DialogTitle className="DialogTitle text-xl mb-5">Create Issue</DialogTitle>
+                            <div className='flex flex-col gap-4 mt-2'>
+                                <div className="grid grid-cols-2">
+                                    <label>Project</label>
+                                    <select {...form.register('project')} displayEmpty onChange={(selectedOption) => { 
+                                        const selectedProject = Number(selectedOption.target.value);
+                                        setSelectedProject(selectedProject);
+                                        setSprintsToSelect(sprints.filter(s => s.Project === selectedProject))
+                                    }}>
+                                        <option hidden selected></option>
+                                        {projects.map(p => <option key={p.project?.ID} value={p.project?.ID}>{p.project?.Name}</option>)}
+                                    </select>
+                                </div>
+                                <div className="grid grid-cols-2 grid-rows-2">
+                                    <label>Summary</label>
+                                    <input {...form.register("summary")} className='rounded-md row-span-2'></input>
+                                </div>
+                                <div className="grid grid-cols-2">
+                                    <label>Story points</label>
+                                    <input inputMode='decimal' {...form.register("storyPoints")} className='rounded-md' type='number'></input>
+                                </div>
+                                <div className="grid grid-cols-2">
+                                    <label>Sprint</label>
+                                    <select {...form.register('sprint')}>
+                                        <option hidden selected></option>
+                                        {sprintsToSelect.map(s => <option key={s.ID} value={s.ID}>{s.Name}</option>)}
+                                    </select>
+                                </div>
+                                <div className="grid grid-cols-2">
+                                    <label>Label</label>
+                                    <select {...form.register('label')}>
+                                        <option hidden selected></option>
+                                        {getLabels().map(l => <option key={l.ID} value={l.ID}>{l.Name}</option>)}
+                                    </select>
+                                </div>
+                                <div className="grid grid-cols-2">
+                                    <label>Assignee</label>
+                                    <select {...form.register('assignee')}>
+                                        <option hidden selected></option>
+                                        {projects.filter(p => p.project?.ID == selectedProject).map(p => p.user ? <option value={p.user?.ID}>{p.user?.Name}</option> : <></>)}
+                                    </select>
+                                </div>
+                                <div className="grid grid-cols-2 grid-rows-5">
+                                    <label>Description</label>
+                                    <textarea {...form.register("description")} className='row-span-5 rounded-md' placeholder='Enter description...'></textarea>
+                                </div>
                             </div>
-                            <div className="grid grid-cols-2 grid-rows-2">
-                                <label>Summary</label>
-                                <input {...form.register("summary")} className='rounded-md row-span-2'></input>
+                            <div className='flex flex-row-reverse gap-10 m-5'>
+                                <DialogClose asChild>
+                                    <button className="flex-end mb-5" onClick={() => submit()}>Save changes</button>
+                                </DialogClose>
+                                <DialogClose asChild>
+                                    <button className="flex-end mb-5" aria-label="Close">
+                                        Close
+                                    </button>
+                                </DialogClose>
                             </div>
-                            <div className="grid grid-cols-2">
-                                <label>Story points</label>
-                                <input inputMode='decimal' {...form.register("storyPoints")} className='rounded-md' type='number'></input>
-                            </div>
-                            <div className="grid grid-cols-2">
-                                <label>Sprint</label>
-                                <select {...form.register('sprint')}>
-                                    {sprintsToSelect.map(s => <option key={s.ID} value={s.ID}>{s.Name}</option>)}
-                                </select>
-                            </div>
-                            <div className="grid grid-cols-2">
-                                <label>Label</label>
-                                <select {...form.register('label')}>
-                                    {getLabels().map(l => <option key={l.ID} value={l.ID}>{l.Name}</option>)}
-                                </select>
-                            </div>
-                            <div className="grid grid-cols-2">
-                                <label>Assignee</label>
-                                <select {...form.register('assignee')}>
-                                    {projects.filter(p => p.project?.ID == selectedProject).map(p => p.user ? <option value={p.user?.ID}>{p.user?.Name}</option> : <></>)}
-                                </select>
-                            </div>
-                            <div className="grid grid-cols-2 grid-rows-5">
-                                <label>Description</label>
-                                <textarea {...form.register("description")} className='row-span-5 rounded-md' placeholder='Enter description...'></textarea>
-                            </div>
-                        </div>
-                        <div className='flex flex-row-reverse gap-10 m-5'>
-                            <Dialog.Close asChild>
-                                <button className="flex-end mb-5" onClick={() => submit()}>Save changes</button>
-                            </Dialog.Close>
-                            <Dialog.Close asChild>
-                                <button className="flex-end mb-5" aria-label="Close">
-                                    Close
-                                </button>
-                            </Dialog.Close>
-                        </div>
-                        </Dialog.Content>
-                        </Dialog.Portal>
-                    </Dialog.Root>
+                        </DialogContent>
+                    </Dialog>
                 </div>
         </form>
         </FormProvider>
